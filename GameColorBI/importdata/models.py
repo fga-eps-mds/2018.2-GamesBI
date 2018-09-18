2cfed25c71aa6ee0692d9005114021ec7e5c3331
Marco Antonio <markinlimac@gmail.com>
2018-09-17 22:53:48 -0300

    [ADD] #120 Add importdata models
    Signed-off-by: Marco Antonio <markinlimac@gmail.com>
    Co-authored-by: Arthur Alves <arthurarp97@gmail.com>

--- GameColorBI/importdata/models.py
+++ GameColorBI/importdata/models.py
@@ -1,3 +1,214 @@
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+
+	id_igdb = models.IntegerField(
+		('Genre ID'),
+		help_text=("Genre id at IGDB"),
+		primary_key=True,
+	)
+
+	name = models.CharField(
+		('Genre name'),
+		help_text=("Genre name"),
+		max_length=100,
+	)
+
+class Stream(models.Model):
+
+	id = models.IntegerField(
+		('Stream ID'),
+		help_text=("Id da stream na Twitch"),
+		primary_key=True,
+	)
+
+	game_id = models.IntegerField(
+		('Game ID'),
+		help_text=("Id do jogo na Twitch")
+	)
+
+	language = models.CharField(
+		('Language'),
+		help_text=("Language of stream"),
+		max_length=100,
+		null=True
+	)
+
+	started_at = models.CharField(
+		('Started date'),
+		help_text=("Date when stream started"),
+		max_length=100,
+		null=True
+	)
+
+	type = models.CharField(
+		('Type'),
+		help_text=("Type of stream"),
+		max_length=100,
+		null=True
+	)
+
+	viewer_count = models.IntegerField(
+		('Viewer count'),
+		help_text=("Number of views in stream"),
+		max_length=100,
+		null=True
+	)
+
+	user = models.ManyToOneField(
+		Utream,
+		blank=True
+	)
+
+class User(models.Model):
+
+	user_id = models.IntegerField(
+		('User ID'),
+		help_text=("User id at Twitch API"),
+		primary_key=True,
+	)
+
+	display_name = models.CharField(
+		('Display name'),
+		help_text=("Display name"),
+		max_length=100,
+		null=True
+	)
+
+	type = models.CharField(
+		('Type'),
+		help_text=("Type"),
+		max_length=100,
+		null=True
+	)
+
+	view_count = models.IntegerField(
+		('Count views'),
+		help_text=("Views couting of user"),
+		null=True
+	)
+
+	follows = models.IntegerField(
+		('follows'),
+		help_text=("Number of followers"),
+		null=True
+	)
+
+
+class Game(models.Model):
+
+	id_igdb = models.IntegerField(
+		('IGDB ID'),
+		help_text=("Id do jogo na IGDB"),
+		primary_key=True,
+	)
+
+	id_steam = models.IntegerField(
+		('STEAM ID'),
+		help_text=("Id do jogo na Steam"),
+		primary_key=True,
+	)
+
+	id_twitch = models.IntegerField(
+		('TWITCH ID'),
+		help_text=("Id do jogo na Twitch"),
+		primary_key=True,
+	)
+
+	name = models.CharField(
+		('Name'),
+		help_text=("Name of game"),
+		max_length=100,
+		null=True
+	)
+
+	psitive_reviews_steam = models.IntegerField(
+		('Positive Reviews'),
+		help_text=("Number of positive reviews at Steam"),
+		null=True
+	)
+
+	negative_reviews_steam = models.IntegerField(
+		('Negative Reviews'),
+		help_text=("Number of negative reviews at Steam"),
+		null=True
+	)
+
+	owners = models.IntegerField(
+		('Owners'),
+		help_text=("Name of the owners"),
+		null=True
+	)
+
+	hypes = models.IntegerField(
+		('Hypes'),
+		help_text=(""),
+		null=True
+	)
+
+	popularity = models.FloatField(
+		('Popularity'),
+		help_text=("Popularity of game"),
+		null=True
+	)
+
+	critics_rating = models.FloatField(
+		('Critics Rating'),
+		help_text=("Rating based on external critic scores"),
+		null=True
+	)
+
+	time_to_beat = models.FloatField(
+		('Time To Beat'),
+		help_text=("Average time to beat the game"),
+		null=True
+	)
+
+	average_forever = models.IntegerField(
+		('Average Forever'),
+		help_text=(""),
+		null=True
+	)
+
+	average_2weeks = models.IntegerField(
+		('Average 2 weeks'),
+		help_text=(""),
+		null=True
+	)
+
+	price = models.IntegerField(
+		('Price'),
+		help_text=("Price of the game"),
+		null=True
+	)
+
+	language = models.CharField(
+		('Languages'),
+		help_text=("Language of the game"),
+		null=True
+	)
+
+	genres = models.ManyToManyField(
+		Genre,
+		blank=True
+	)
+
+	Streams = models.OneToManyField(
+		Stream,
+		blank=True
+	)
+
+	def __str__(self):
+	    """
+	    Returns the object as a string, the attribute that will represent
+	    the object.
+	    """
+
+	    return self.name
+
+	class Meta:
+	    """
+	    Some information about feedback class.
+	    """
+	    verbose_name = ("Game BI")
+	    verbose_name_plural = ("Games BI")
