029e2d0bd5432a90a7fe500ea56affce0f4b1fba
Bruno Rodrigues <bruesmanbruesman@hotmail.com>
2018-09-24 10:36:27 -0300

    [HOTFIX] Updated folders structure and project name

--- CrossData/settings.py
+++ CrossData/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'GameColorBI.urls'
+ROOT_URLCONF = 'CrossData.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'GameColorBI.wsgi.application'
+WSGI_APPLICATION = 'CrossData.wsgi.application'
 
 
 # Database
---
+++ makefiles/docker.mk
@@ -0,0 +1,54 @@
+# DOCKER DEPLOY ------------------------------------------------
+file := "docker-compose.yml"
+
+up:
+	# Create and start containers
+	sudo docker-compose up
+
+build:
+	# Rebuild the docker compose
+	sudo docker-compose build
+
+restart:
+	# Restart services
+	sudo docker-compose restart
+
+logs:
+	# View output from containers
+	sudo docker-compose logs
+
+start:
+	# Start services
+	sudo docker-compose start
+
+stop:
+	# Stop services
+	sudo docker-compose stop
+
+ps:
+	# List all running containers
+	sudo docker-compose ps
+
+down:
+	# Stop and Remove all containers
+	sudo docker-compose down
+
+migrations:
+	# Create migrations from importdata models
+	sudo docker-compose run crossweb python manage.py makemigrations importdata
+
+migrate:
+	# Migrate migrations from importdata on database
+	sudo docker-compose run crossweb python manage.py migrate importdata
+
+fixture:
+	# Generate fixtures saved upon importdata models
+	sudo docker-compose run crossweb python manage.py dumpdata importdata --format json > CrossData/importdata/fixtures/data.json
+
+loaddata:
+	# load fixtures saved upon importdata models
+	sudo docker-compose run crossweb python manage.py loaddata data.json
+
+test:
+	# run unit tests
+	sudo docker-compose run crossweb python manage.py test
