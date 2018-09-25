a50bacb9752599e65b02c85d172487f0fceba9f8
Bruno Rodrigues <bruesmanbruesman@hotmail.com>
2018-09-25 10:30:13 -0300

    [UPDATED] #121 Updated models to merge all data into one single model

--- CrossData/importdata/models.py
+++ CrossData/importdata/models.py
@@ -1,54 +1,6 @@
 from django.db import models
 
-class Genre(models.Model):
-
-	id_igdb = models.IntegerField(
-		('Genre ID'),
-		help_text=("Genre id at IGDB"),
-		primary_key=True,
-	)
-
-	name = models.CharField(
-		('Genre name'),
-		help_text=("Genre name"),
-		max_length=100,
-	)
-
-class User(models.Model):
-
-	user_id = models.IntegerField(
-		('User ID'),
-		help_text=("User id at Twitch API"),
-		primary_key=True,
-	)
-
-	display_name = models.CharField(
-		('Display name'),
-		help_text=("Display name"),
-		max_length=100,
-		null=True
-	)
-
-	type = models.CharField(
-		('Type'),
-		help_text=("Type"),
-		max_length=100,
-		null=True
-	)
-
-	view_count = models.IntegerField(
-		('Count views'),
-		help_text=("Views couting of user"),
-		null=True
-	)
-
-	follows = models.IntegerField(
-		('follows'),
-		help_text=("Number of followers"),
-		null=True
-	)
-
-class Game(models.Model):
+class GeneralData(object):
 
 	id_igdb = models.IntegerField(
 		('IGDB ID'),
@@ -70,26 +22,15 @@ class Game(models.Model):
 
 	name = models.CharField(
 		('Name'),
-		help_text=("Name of game"),
+		help_text=("Game name"),
 		max_length=100,
 		null=True
 	)
 
-	positive_reviews_steam = models.IntegerField(
-		('Positive Reviews'),
-		help_text=("Number of positive reviews at Steam"),
-		null=True
-	)
-
-	negative_reviews_steam = models.IntegerField(
-		('Negative Reviews'),
-		help_text=("Number of negative reviews at Steam"),
-		null=True
-	)
-
-	owners = models.IntegerField(
-		('Owners'),
-		help_text=("Name of the owners"),
+	#IGDB
+	time_to_beat = models.FloatField(
+		('Time To Beat'),
+		help_text=("Average time to beat the game"),
 		null=True
 	)
 
@@ -111,21 +52,34 @@ class Game(models.Model):
 		null=True
 	)
 
-	time_to_beat = models.FloatField(
-		('Time To Beat'),
-		help_text=("Average time to beat the game"),
+	#Steam
+	positive_reviews_steam = models.IntegerField(
+		('Steam Positive Reviews'),
+		help_text=("Number of positive reviews at Steam"),
+		null=True
+	)
+
+	negative_reviews_steam = models.IntegerField(
+		('Steam Negative Reviews'),
+		help_text=("Number of negative reviews at Steam"),
+		null=True
+	)
+
+	owners = models.IntegerField(
+		('Steam Owners'),
+		help_text=("Name of the owners"),
 		null=True
 	)
 
 	average_forever = models.IntegerField(
-		('Average Forever'),
+		('Average Played Time'),
 		help_text=("Average forever of the game"),
 		null=True
 	)
 
 	average_2weeks = models.IntegerField(
-		('Average 2 weeks'),
-		help_text=("Average forever of the game"),
+		('Average Played Time 2 weeks'),
+		help_text=("Average Played Time 2 weeks of the game"),
 		null=True
 	)
 
@@ -142,38 +96,81 @@ class Game(models.Model):
 		max_length=150
 	)
 
-	genres = models.ManyToManyField(
-		Genre
+	genres = models.CharField(
+		('Game Genre'),
+		help_text=("Name of the Game Genre"),
+		max_length=50,
+		null=True
 	)
 
 	#YouTube
 	count_videos = models.IntegerField(
-		('count_videos'),
+		('Youtube total videos number'),
 		help_text=("Number of videos found in youtube request"),
 		null=True
 	)
 
 	count_views = models.IntegerField(
-		('count_views'),
+		('Youtube total views'),
 		help_text=("Number of views summed from youtube videos"),
 		null=True
 	)
 
 	count_likes = models.IntegerField(
-		('count_likes'),
+		('Youtube total likes'),
 		help_text=("Number of likes summed from youtube videos"),
 		null=True
 	)
 
 	count_dislikes = models.IntegerField(
-		('count_dislikes'),
+		('Youtube total dislikes'),
 		help_text=("Number of dislikes summed from youtube videos"),
 		null=True
 	)
 
 	count_comments = models.IntegerField(
-		('count_comments'),
+		('Youtube total comments count'),
 		help_text=("Number of comments summed from youtube videos"),
+		null=True
+	)
+
+	#Twitch
+	view_count = models.IntegerField(
+		('Twitch users total views'),
+		help_text=("Total Views couting of twitch users"),
+		null=True
+	)
+
+	follows = models.IntegerField(
+		('Twitch users total follows'),
+		help_text=("Number of followers"),
+		null=True
+	)
+
+	language = models.CharField(
+		('Twitch stream Language'),
+		help_text=("Language of stream"),
+		max_length=100,
+		null=True
+	)
+
+	started_at = models.CharField(
+		('Twitch stream Started date'),
+		help_text=("Date when stream started"),
+		max_length=100,
+		null=True
+	)
+
+	type = models.CharField(
+		('Twitch stream Type'),
+		help_text=("Type of stream"),
+		max_length=100,
+		null=True
+	)
+
+	viewer_count = models.IntegerField(
+		('Twitch Game streams Viewer count'),
+		help_text=("Number of views in stream"),
 		null=True
 	)
 
@@ -189,56 +186,5 @@ class Game(models.Model):
 	    """
 	    Some information about feedback class.
 	    """
-	    verbose_name = ("Game BI")
-	    verbose_name_plural = ("Games BI")
-
-
-class Stream(models.Model):
-
-	id = models.IntegerField(
-		('Stream ID'),
-		help_text=("Id da stream na Twitch"),
-		primary_key=True,
-	)
-
-	# game_id = models.IntegerField(
-	# 	('Game ID'),
-	# 	help_text=("Id do jogo na Twitch")
-	# )
-
-	language = models.CharField(
-		('Language'),
-		help_text=("Language of stream"),
-		max_length=100,
-		null=True
-	)
-
-	started_at = models.CharField(
-		('Started date'),
-		help_text=("Date when stream started"),
-		max_length=100,
-		null=True
-	)
-
-	type = models.CharField(
-		('Type'),
-		help_text=("Type of stream"),
-		max_length=100,
-		null=True
-	)
-
-	viewer_count = models.IntegerField(
-		('Viewer count'),
-		help_text=("Number of views in stream"),
-		null=True
-	)
-
-	user = models.ForeignKey(
-		User,
-		on_delete=models.CASCADE
-	)
-
-	game = models.ForeignKey(
-		Game,
-		on_delete=models.CASCADE
-	)
+	    verbose_name = ("Game Data")
+	    verbose_name_plural = ("Games Data")
