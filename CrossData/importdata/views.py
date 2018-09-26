dfefb6108a01094e8dca83c502ab284948626168
Alan Lima <alanronison.lima@gmail.com>
2018-09-26 14:22:38 -0300

    [ADD] #121 Created request and merge to twitch service

--- CrossData/importdata/views.py
+++ CrossData/importdata/views.py
@@ -21,19 +21,18 @@ class GamesView(APIView):
         GeneralData.objects.all().delete()
         games_igdb = self.get_games_igdb()
         games_steam = self.get_games_steam()
-        # games_twich = self.get_games_twich()
+        games_twich = self.get_games_twich()
         games_youtube = self.get_games_youtube()
 
         dataFrame_igdb = self.filter_games_igdb(games_igdb)
         dataFrame_steam = self.filter_games_steam(games_steam)
-        # dataFrame_twich = self.filter_games_twitch(games_twich)
+        dataFrame_twich = self.filter_games_twitch(games_twich)
         dataFrame_youtube = self.filter_games_youtube(games_youtube)
 
-        dataframe_all = self.merge_dataFrames(dataFrame_igdb, dataFrame_steam, dataFrame_youtube)
-        print(dataframe_all)
+        dataframe_all = self.merge_dataFrames(dataFrame_igdb, dataFrame_steam, dataFrame_youtube, dataFrame_twich)
         self.save_games(dataframe_all)
 
-        return Response(data=dataframe_all)
+        return Response(data=games_igdb)
 
 
     def get_games_igdb(self):
@@ -65,6 +64,7 @@ class GamesView(APIView):
 
 
     def filter_games_igdb(self, games_igdb):
+        array_id = []
         array_name = []
         array_time_to_beat = []
         array_hypes = []
@@ -73,25 +73,31 @@ class GamesView(APIView):
         array_genres = []
 
         for game in games_igdb:
+            array_id.append(game['id'])
             array_name.append(game['name'])
             array_time_to_beat.append(game['time_to_beat'])
             array_hypes.append(game['hypes'])
             array_popularity.append(game['popularity'])
             array_critics_rating.append(game['aggregated_rating'])
-            # array_genres.append(game['genres'][0]['name'])
+            if(len(game['genres']) > 0):
+                array_genres.append(game['genres'][0]['name'])
+            else:
+                array_genres.append(None)
 
         filtered_game = {
+            'id': array_id,
             'name': array_name,
             'time_to_beat': array_time_to_beat,
             'hypes': array_hypes,
             'popularity': array_popularity,
-            'critics_rating': array_critics_rating
-            # 'genres': array_genres
+            'critics_rating': array_critics_rating,
+            'genres': array_genres
         }
         return pd.DataFrame(filtered_game)
 
 
     def filter_games_youtube(self, games_youtube):
+        array_id = []
         array_name = []
         array_count_videos = []
         array_count_views = []
@@ -100,6 +106,7 @@ class GamesView(APIView):
         array_count_comments = []
 
         for game in games_youtube:
+            array_id.append(game['id'])
             array_name.append(game['name'])
             array_count_videos.append(game['count_videos'])
             array_count_views.append(game['count_views'])
@@ -108,6 +115,7 @@ class GamesView(APIView):
             array_count_comments.append(game['count_comments'])
 
         filtered_game = {
+            'id': array_id,
             'name': array_name,
             'count_videos': array_count_videos,
             'count_views': array_count_views,
@@ -121,26 +129,16 @@ class GamesView(APIView):
 
     def filter_games_twitch(self, games_twitch):
         array_name = []
-        array_language = []
-        array_started_at = []
-        array_type = []
-        array_viewer_count = []
+        array_views = []
 
         for game in games_twitch:
             array_name.append(game['game_name'])
-            array_language.append(game['language'])
-            array_started_at.append(game['started_at'])
-            array_type.append(game['type'])
-            array_viewer_count.append(game['viewer_count'])
+            array_views.append(game['total_views'])
 
         filtered_game = {
             'name': array_name,
-            'language': array_language,
-            'started_at': array_started_at,
-            'type': array_type,
-            'viewer_count': array_viewer_count
+            'views': array_views,
         }
-
         return pd.DataFrame(filtered_game)
 
 
@@ -153,9 +151,10 @@ class GamesView(APIView):
         array_average_forever = []
         array_average_2weeks = []
         array_price = []
-        # array_average_forever = []
+        # array_lenguages = []
 
         for game in games_steam:
+            array_id.append(game['id'])
             array_name.append(game['name'])
             array_positive_reviews_steam.append(game['positive_reviews_steam'])
             array_negative_reviews_steam.append(game['negative_reviews_steam'])
@@ -163,8 +162,10 @@ class GamesView(APIView):
             array_average_forever.append(game['average_forever'])
             array_average_2weeks.append(game['average_2weeks'])
             array_price.append(game['price'])
+            # array_lenguages.append(game['lenguages'])
 
         filtered_game = {
+            'id': array_id,
             'name': array_name,
             'positive_reviews_steam': array_positive_reviews_steam,
             'negative_reviews_steam': array_negative_reviews_steam,
@@ -172,17 +173,17 @@ class GamesView(APIView):
             'average_forever': array_average_forever,
             'average_2weeks': array_average_2weeks,
             'price': array_price
-            # 'lenguages': None
+            # 'lenguages_game': array_lenguages
         }
 
         return pd.DataFrame(filtered_game)
 
 
-    def merge_dataFrames(self, frame_igdb, frame_steam, frame_youtube):
+    def merge_dataFrames(self, frame_igdb, frame_steam, frame_youtube, frame_twich):
 
         merge_one = pd.merge(frame_igdb, frame_youtube, how='left', on='name')
-        merge_all = pd.merge(merge_one, frame_steam, how='left', on='name')
-        # merge_all = pd.merge(merge_two, frame_youtube, how='left', on='name')
+        merge_two = pd.merge(merge_one, frame_steam, how='left', on='name')
+        merge_all = pd.merge(merge_two, frame_twich, how='left', on='name')
 
         return merge_all
 
@@ -199,11 +200,14 @@ class GamesView(APIView):
         dataframe_all['owners'].fillna(0, inplace=True)
         dataframe_all['average_forever'].fillna(0, inplace=True)
         dataframe_all['average_2weeks'].fillna(0, inplace=True)
+        dataframe_all['genres'].fillna("", inplace=True)
+        # dataframe_all['lenguages_game'].fillna("", inplace=True)
         dataframe_all['count_videos'].fillna(0, inplace=True)
         dataframe_all['count_views'].fillna(0, inplace=True)
         dataframe_all['count_likes'].fillna(0, inplace=True)
         dataframe_all['count_dislikes'].fillna(0, inplace=True)
         dataframe_all['count_comments'].fillna(0, inplace=True)
+        dataframe_all['views'].fillna(0, inplace=True)
 
         return dataframe_all
 
@@ -213,25 +217,6 @@ class GamesView(APIView):
         count = 0
         while count < number_rows:
             game = dataframe.loc[count]
-            print('nome: ' + str(game['name']))
-            print('time_to_beat: ' + str(game.time_to_beat))
-            print('hypes: ' + str(game.hypes))
-            print('popularity: ' + str(game.popularity))
-            print('critics_rating: ' + str(game.critics_rating))
-            print('positive_reviews_steam: ' + str(game.positive_reviews_steam))
-            print('negative_reviews_steam: ' + str(game.negative_reviews_steam))
-            print('owners: ' + str(game.owners))
-            print('average_forever: ' + str(game.average_forever))
-            print('average_2weeks: ' + str(game.average_2weeks))
-            print('price: ' + str(game.price))
-            print('count_videos: ' + str(game.count_videos))
-            print('count_views: ' + str(game.count_views))
-            print('count_likes: ' + str(game.count_likes))
-            print('count_dislikes: ' + str(game.count_dislikes))
-            print('count_comments: ' + str(game.count_comments))
-            print("\n--------\n---------\n")
-
-
             new_game = GeneralData(
                 # id_igdb
             	# id_steam
@@ -241,20 +226,20 @@ class GamesView(APIView):
             	hypes=int(game['hypes']),
             	popularity=float(game['popularity']),
             	critics_rating=float(game['critics_rating']),
-            	# genres
+            	genres=str(game['genres']),
             	positive_reviews_steam=int(game['positive_reviews_steam']),
             	negative_reviews_steam=int(game['negative_reviews_steam']),
             	owners=int(game['owners']),
             	average_forever=int(game['average_forever']),
             	average_2weeks=int(game['average_2weeks']),
             	price=int(game['price']),
-            	# language
+            	# languages_game=str(game['languages_game']),
             	count_videos=int(game['count_videos']),
             	count_views=int(game['count_views']),
             	count_likes=int(game['count_likes']),
             	count_dislikes=int(game['count_dislikes']),
             	count_comments=int(game['count_comments']),
-            	# view_count
+            	view_count=int(game['views'])
             	# follows
             	# language
             	# started_at
