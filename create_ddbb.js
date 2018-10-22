41e5b9516d6cc9082a5360e102fcf391e4de5ea1
Bruno Rodrigues <bruesmanbruesman@hotmail.com>
2018-10-22 14:49:28 -0300

    [DELETE] #201 #206 Deleted unnecessary files

--- create_ddbb.js
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE gamesbi;
-CREATE USER gamesbi WITH PASSWORD 'gamesbi123';
-ALTER ROLE gamesbi SET client_encoding TO 'utf8';
-ALTER ROLE gamesbi SET timezone TO 'UTC';
-GRANT ALL PRIVILEGES ON DATABASE gamesbi TO gamesbi;
