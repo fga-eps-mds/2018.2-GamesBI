f089170e8d4a0b35fe5e16fe800f30b123eaf7f7
augustovilarins <augusto.vilarins@gmail.com>
2018-10-23 23:48:07 -0300

    [ADD] #204 Add database urls for searging games
    Co-authored-by: Ingrid <ingr.dcg@gmail.com>

--- CrossData/importdata/views.py
+++ CrossData/importdata/views.py
@@ -1,11 +1,16 @@
 import requests
 
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 # from .serializers import GameSerializer
 
 import pandas as pd
 
-
 class GamesView(APIView):
-    pass
+
+    @api_view(('GET',))
+    def search_game(request, name):
+        data = {'nome': name}
+        return Response(data, status=status.HTTP_200_OK)
