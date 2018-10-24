06481a80cd7afde580fac01a9df77655aa1cd9ef
Bruno Rodrigues <bruesmanbruesman@hotmail.com>
2018-10-23 22:34:51 -0300

    [ADD] #200 Added Serializers
    Co-authored-by: Lucas Machado <lmlucasmachadom@gmail.com>

--- CrossData/importdata/serializers.py
+++ CrossData/importdata/serializers.py
@@ -7,3 +7,43 @@ class GameSerializer(serializers.ModelSerializer):
 
 		model = Game
 		fields = '__all__'
+
+
+class YoutubeInfoSerializer(serializers.ModelSerializer):
+
+	game = GameSerializer()
+
+	class Meta:
+
+		model = InfoYoutube
+		fields = '__all__'
+
+
+class SteamInfoSerializer(serializers.ModelSerializer):
+
+	game = GameSerializer()
+
+	class Meta:
+
+		model = InfoSteam
+		fields = '__all__'
+
+
+class TwitchInfoSerializer(serializers.ModelSerializer):
+
+	game = GameSerializer()
+
+	class Meta:
+
+		model = InfoTwitch
+		fields = '__all__'
+
+
+class TwitchStreamSerializer(serializers.ModelSerializer):
+
+	game = GameSerializer()
+
+	class Meta:
+
+		model = TwitchStream
+		fields = '__all__'
