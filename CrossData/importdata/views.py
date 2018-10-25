839d226cbb511b7bcdbe2fec5f3c2fcfc979c9af
augustovilarins <augusto.vilarins@gmail.com>
2018-10-25 13:51:17 -0300

    [ADD] #204 Add search method to the view.py file
    Co-authored-by: Ingrid <ingr.dcg@gmail.com>

--- CrossData/importdata/views.py
+++ CrossData/importdata/views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
+
 # from .serializers import GameSerializer
 
 import pandas as pd
@@ -11,6 +12,8 @@ import pandas as pd
 class GamesView(APIView):
 
     @api_view(('GET',))
-    def search_game(request, name):
-        data = {'nome': name}
-        return Response(data, status=status.HTTP_200_OK)
+    def search_game(request, nome):
+        data = {'name': nome}
+        queryset = Game.objects.filter(name=nome)
+        serializer = self.get_serializer(queryset,many=True)
+    return Response(serializer.data,status=status.HTTP_200_OK)
