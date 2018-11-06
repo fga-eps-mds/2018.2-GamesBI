35700c77251c30f75ddb7128b43fa884623aafac
Nukdown <belchior.emanoel@gmail.com>
2018-11-05 23:02:30 -0200

    [DELETE] #238 Solve conflits and remove metabase install configs

--- CrossData/urls.py
+++ CrossData/urls.py
@@ -18,6 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('metabase/', include('CrossData.metabase.urls')),
     path('api/', include('CrossData.API.urls'))
 ]
