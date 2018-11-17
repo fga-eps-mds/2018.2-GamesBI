7fc05787b8cadd907c78a917c2e7b15f9ea56a4f
Arthur <arthurarp97@gmail.com>
2018-11-17 11:58:50 -0200

    [ADD] #252 Add test to verify if graphic does not return null
    Co-authored-by: Lucas <lmlucasmachadom@gmail.com>
    Co-authored-by: Marco <markinlimac@gmail.com>

--- CrossData/API/tests_get_data.py
+++ CrossData/API/tests_get_data.py
@@ -7,8 +7,85 @@ class EndpointGETTestCase(APITestCase):
 
 	def setUp(self):
 
+		self.data = [
+	{
+		"id_steam": 123,
+		"name": "Test 1",
+		"positive_reviews_steam": 1923123,
+		"negative_reviews_steam": 12121,
+		"owners": 130000,
+		"average_forever": 2127,
+		"average_2weeks": 132,
+		"price": "0",
+		"languages": [
+			"mandarim", "espanhol"
+		],
+		"genres": [
+			"tiro", "porrada"
+		],
+		"main_image": "google.com",
+		"screenshots": [
+			{
+				"url": "https://steamcdn-a.akamaihd.net/steam/apps/570/ss_86d675fdc73ba10462abb8f5ece7791c5047072c.600x338.jpg?t=1536248487",
+				"palette": [
+					{
+						"r": 8,
+						"g": 16,
+						"b": 2,
+						"hex": "#1aa741"
+					},
+					{
+						"r": 34,
+						"g": 12,
+						"b": 37,
+						"hex": "#2e204d"
+					},
+					{
+						"r": 22,
+						"g": 48,
+						"b": 34,
+						"hex": "#484454"
+					},
+					{
+						"r": 121,
+						"g": 80,
+						"b": 254,
+						"hex": "#b5b49a"
+					},
+					{
+						"r": 19,
+						"g": 26,
+						"b": 21,
+						"hex": "#3b4233"
+					}
+				]
+			},
+		],
+		"release_date": "1 Feb, 1999",
+		"r_average": 83,
+		"g_average": 82,
+		"b_average": 74,
+		"count_videos": 1,
+		"count_views": 2609773,
+		"count_likes": 5555,
+		"count_dislikes": 1107,
+		"count_comments": 4152,
+		"total_views": 46939,
+		"streams": [
+			{
+				"language": "en",
+				"game_id": "29595",
+				"started_at": "2018-11-03T12:00:06Z",
+				"type": "live",
+				"viewer_count": 23661
+			},
+		]
+	},
+]
+
 		self.url_graphic = reverse("get_data", kwargs={'graphtype':'line', 'yaxis':'average_2weeks', 'xaxis':'games'})
 		self.url_table = reverse("get_table_data", kwargs={'table_type':'trendingnow'})
+		self.url = reverse("games_view")
 
 	def tearDown(self):
 
@@ -29,3 +106,12 @@ class EndpointGETTestCase(APITestCase):
 
 		response = self.client.get(self.url_table)
 		self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+	def test_graphic_return_not_null(self):
+
+		json_ideal_return = {'x_axis': [], 'y_axis': []}
+
+		self.client.post(self.url, self.data, format='json')
+		response = self.client.get(self.url_graphic)
+		self.assertNotEqual(response.data, json_ideal_return)
